337110ad83222e969b79d6444b6030046ab94beb
Fellipe Vasconcelos da Silva <fellipe.fvs@gmail.com>
2012-08-08 20:29:17 -0300

    Versão inicial
    Versão inicial do taxi-android

---
+++ gen/tcc/iesgo/activity/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package tcc.iesgo.activity;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/tcc/iesgo/activity/ClientMapActivity.java
@@ -0,0 +1,13 @@
+package tcc.iesgo.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ClientMapActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
